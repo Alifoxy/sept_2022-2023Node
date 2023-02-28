56f20eb2c3395a67a975cfbc216369f8f834d1b6
Alifoxy <nightfoxy666@gmail.com>
2023-02-28 18:29:39 +0300

    first

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-fs.mkdir
+const fs =require('fs');
+const path =require('path');
+
+
+fs.mkdir(path.join(__dirname, 'main' ),(error) => {
+    if (error) throw new Error(error.message)
+})
+
+for (let i = 1; i <= 5; i++) {
+    fs.mkdir(path.join(__dirname,'main',`dir ${i}` ),(error) => {
+        if (error) throw new Error(error.message)
+    })
+    fs.appendFile(path.join(__dirname,'main',`file ${i}.txt` ),`text ${i}`,(error) => {
+        if (error) throw new Error(error.message)
+    })
+}
