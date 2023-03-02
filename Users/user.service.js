09c4d0f5fa983a47ceda3a29b3ec3a686afbdbc0
Alifoxy <nightfoxy666@gmail.com>
2023-03-03 00:02:09 +0300

    first

---
+++ Users/user.service.js
@@ -0,0 +1,6 @@
+const path = require("path");
+const fs = require("fs");
+
+fs.readFile(path.join(__dirname, 'users.json','users' ),async (error, data) => {
+    await users.write
+})
--- app.js
+++ app.js
@@ -1,22 +1,149 @@
-const fs =require('fs');
-const path =require('path');
+// const fs =require('fs');
+// const path =require('path');
+//
+//
+// fs.mkdir(path.join(__dirname, 'main' ),(error) => {
+//     if (error) throw new Error(error.message)
+// })
+//
+// for (let i = 1; i <= 5; i++) {
+//     fs.mkdir(path.join(__dirname,'main',`dir ${i}` ),(error) => {
+//         if (error) throw new Error(error.message)
+//     })
+//     fs.appendFile(path.join(__dirname,'main',`file ${i}.txt` ),`text ${i}`,(error) => {
+//         if (error) throw new Error(error.message)
+//     })
+// }
+//
+//
+// fs.readdir(path.join(__dirname, 'main' ),(error, data) => {
+//     if (error) throw new Error(error.message)
+//     console.log(data);
+// })
+
+// -- EVENT --
+// const event = require('node:events');
+//
+// const eventEmitter = new event();
+//
+// eventEmitter.on('click', ()=>{
+//   console.log('Click click click');
+// });
+//
+// eventEmitter.emit('click')
+//
+// eventEmitter.emit('click')
+// eventEmitter.emit('click')
+// eventEmitter.emit('click')
+//
+// eventEmitter.once('clickAndDie', ()=>{
+//   console.log("I'm gonna die after being called");
+// })
+// console.log(eventEmitter.eventNames());
+//
+// eventEmitter.emit('clickAndDie');
+//
+// eventEmitter.emit('clickAndDie');
+// eventEmitter.emit('clickAndDie');
+// eventEmitter.emit('clickAndDie');
+// eventEmitter.emit('clickAndDie');
+//
+// console.log(eventEmitter.eventNames());
 
 
-fs.mkdir(path.join(__dirname, 'main' ),(error) => {
-    if (error) throw new Error(error.message)
+// -- STREAMS --
+// const fs = require('fs');
+// const path = require("path");
+//
+// const readStream = fs.createReadStream(path.join('test', 'text3.txt'));
+// const writeStream = fs.createWriteStream(path.join('test', 'text2.txt'))
+
+// read, write, duplex, transform --- types of streams !!!
+
+// readStream.on('data', (chunk) => {
+//   writeStream.write(chunk);
+// });
+
+// const handleError = () => {
+//   console.error('ERROR!!!');
+//   readStream.destroy();
+//   writeStream.end('ERROR WHILE READING FILE');
+// }
+//
+// readStream
+//   .on('error', handleError)
+//   .pipe(writeStream)
+//   .on('error', handleError);
+
+// -- EXPRESS --
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/users', (req, res)=>{
+    res.json(users);
+});
+
+app.get('/users/:userId', (req, res)=>{
+    const { userId } = req.params;
+    const user = users[+userId];
+
+    res.json(user);
+});
+
+app.post('/users', (req, res)=>{
+    const body = req.body;
+    users.push(body);
+
+    res.status(201).json({
+        message: 'User created!'
+    })
 })
 
-for (let i = 1; i <= 5; i++) {
-    fs.mkdir(path.join(__dirname,'main',`dir ${i}` ),(error) => {
-        if (error) throw new Error(error.message)
-    })
-    fs.appendFile(path.join(__dirname,'main',`file ${i}.txt` ),`text ${i}`,(error) => {
-        if (error) throw new Error(error.message)
-    })
-}
 
+app.put('/users/:userId', (req, res)=>{
+    const { userId } = req.params;
+    const {updatedUser} = req.body;
 
-fs.readdir(path.join(__dirname, 'main' ),(error, data) => {
-    if (error) throw new Error(error.message)
-    console.log(data);
+    users[+userId] = updatedUser;
+
+    res.status(200).json({
+        message: 'User updated',
+        data: users[+userId]
+    })
 })
+
+app.delete('/users/:userId', (req, res)=>{
+    const { userId } = req.params;
+        if (!userId) {
+            console.error('User was not found!');
+        }else {
+            users.splice(+userId, 1);
+
+            res.status(200).json({
+                message: 'User deleted',
+            })
+        }
+
+})
+
+app.get('/welcome', (req, res)=>{
+    res.send('WELCOME');
+});
+
+
+// app.post()
+// app.put()
+// app.patch()
+// app.delete()
+
+const PORT = 5100;
+
+app.listen(PORT, ()=>{
+    console.log(`Server has started on PORT ${PORT} 🚀`);
+});
